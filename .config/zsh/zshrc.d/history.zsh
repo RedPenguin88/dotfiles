ab726d8606c025fc77d360ffe932d8e2b59bfa0f
RedPenguin88 <ethanweihunt@gmail.com>
2022-06-11 11:07:13 -0700

    set basic hist options

--- .config/zsh/zshrc.d/history.zsh
+++ .config/zsh/zshrc.d/history.zsh
@@ -5,3 +5,12 @@
 HISTFILE="$XDG_DATA_HOME"/zsh/history
 HISTSIZE=10000
 SAVEHIST=10000
+
+# Let multiple zsh sessions share the same history
+setopt SHARE_HISTORY
+
+# Removes copies of lines still in the history list, keeping only the newly added one
+setopt HIST_IGNORE_ALL_DUPS
+
+# Do not save duplicated lines more than once
+setopt HIST_SAVE_NO_DUPS
