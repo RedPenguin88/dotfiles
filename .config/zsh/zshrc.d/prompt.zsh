bbabfe8e135af621324c029668581446e8e498e5
RedPenguin88 <ethanweihunt@gmail.com>
2022-06-14 13:34:41 -0700

    created prompt.zsh

---
+++ .config/zsh/zshrc.d/prompt.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+##
+# Basic Prompt Settings
+#
+# TODO: Integrate git status into prompt
+#
+
+autoload -U colors && colors
+PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
