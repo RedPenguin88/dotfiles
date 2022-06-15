90cf6e1814eafc95d9756884a34c1a00ad255cb4
RedPenguin88 <ethanweihunt@gmail.com>
2022-06-15 12:48:48 -0700

    colorize commands when possible

--- .config/zsh/zshrc.d/alias.zsh
+++ .config/zsh/zshrc.d/alias.zsh
@@ -9,3 +9,9 @@ alias vim='nvim'
 
 # Use exa (ls replacement) for colors, icons, and other niceties
 alias ls='exa --group-directories-first --color=auto --icons'
+
+# Use color for commands when possible
+alias \
+	grep='grep --color=auto' \
+	diff='diff --color=auto' \
+	cat='bat --paging=never'
