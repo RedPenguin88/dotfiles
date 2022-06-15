b481399c0be37a83d8a3a84f1b23d3422959def4
RedPenguin88 <ethanweihunt@gmail.com>
2022-06-15 13:07:38 -0700

    add syntax highlighting

---
+++ .config/zsh/zshrc.d/plugins.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+##
+# Setup and source various plugins here
+#
+# TODO: Handle plugins distro-agnostically
+#
+
+. /usr/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh 2>/dev/null
