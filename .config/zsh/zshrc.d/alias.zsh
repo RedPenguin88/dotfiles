2514e23e3fe91b3f59884a8c2cb52807ee63161c
RedPenguin88 <ethanweihunt@gmail.com>
2022-06-15 12:47:04 -0700

    create alias.zsh

---
+++ .config/zsh/zshrc.d/alias.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+##
+# Set Basic aliases for convenience
+#
+
+# Use nvim instead of vim
+alias vim='nvim'
+
+# Use exa (ls replacement) for colors, icons, and other niceties
+alias ls='exa --group-directories-first --color=auto --icons'
