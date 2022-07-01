7e9076096c5b18bdbe9f729ce5dd50e5f4a1330d
RedPenguin88 <ethanweihunt@gmail.com>
2022-07-01 09:58:54 -0700

    use vim keybinds for menu navigation

--- .config/zsh/zshrc.d/keybinds.zsh
+++ .config/zsh/zshrc.d/keybinds.zsh
@@ -5,3 +5,9 @@
 # 
 
 bindkey -s '^o' 'lfcd\n'    # File navigation using lf (terminal file manager)
+
+# Navigate completion menu using vim keys
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
