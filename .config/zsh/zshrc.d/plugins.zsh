a58f590abaf6d152b8d67163574de5f3e230e6a3
RedPenguin88 <ethanweihunt@gmail.com>
2022-07-15 11:14:09 -0700

    use znap plugin manager for plugins

--- .config/zsh/zshrc.d/plugins.zsh
+++ .config/zsh/zshrc.d/plugins.zsh
@@ -3,7 +3,29 @@
 ##
 # Setup and source various plugins here
 #
-# TODO: Handle plugins distro-agnostically
-#
 
-. /usr/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh 2>/dev/null
+# Feel free to change the git directory to whatever you prefer.
+local znap=~/.local/src/zsh-snap/znap.zsh
+
+# Clone Znap plugin manager if it doesn't exist
+if ! [[ -r $znap ]]; then
+  mkdir -p ~/.local/src
+  git -C ~/.local/src clone --depth 1 -- \
+      https://github.com/marlonrichert/zsh-snap.git
+fi
+
+# Source Znap, a neat plugin manager with legible syntax and some neat features
+. $znap
+
+# List of plugins
+local -a plugins=(
+    zdharma-continuum/fast-syntax-highlighting  # Command-line syntax highlighting
+    zsh-users/zsh-autosuggestions               # Inline suggestions
+)
+
+znap clone $plugins
+
+local p
+for p in $plugins; do
+  znap source $p
+done
