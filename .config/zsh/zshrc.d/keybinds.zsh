160430bedbfcb6cab6d8926708511e4fb433c334
RedPenguin88 <ethanweihunt@gmail.com>
2022-07-15 09:38:28 -0700

    fix vi-mode and change formatting

--- .config/zsh/zshrc.d/keybinds.zsh
+++ .config/zsh/zshrc.d/keybinds.zsh
@@ -4,10 +4,15 @@
 # Set basic keybinds
 # 
 
-bindkey -s '^o' 'lfcd\n'    # File navigation using lf (terminal file manager)
+# File navigation using lf (terminal file manager)
+bindkey -s '^o' 'lfcd\n'
 
 # Navigate completion menu using vim keys
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
+
+# This fixes strange behavior in vi-mode wherein backspace doesn't delete
+# characters after exiting normal mode
+bindkey "^?" backward-delete-char
