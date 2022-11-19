e947d3605004355aab50c8d2a784ee4ee8de8ed5
Ethan Hunt <ethanweihunt@gmail.com>
2022-11-18 21:17:06 -0800

    add aliases for commonly used commands

--- .config/zsh/zshrc.d/alias.zsh
+++ .config/zsh/zshrc.d/alias.zsh
@@ -3,29 +3,38 @@
 ##
 # Set Basic aliases for convenience
 # 
-# Included are settings for visual clarity and 'safe' options to prevent
+# Included are settings for visual clarity and "safe" options to prevent
 # unfortunate accidents
 # 
 
 # Use nvim instead of vim
-alias vim='nvim'
+alias vim="nvim"
 
 # Use exa (ls replacement) for colors, icons, and other niceties
-alias ls='exa --group-directories-first --color=auto --icons'
+alias ls="exa --group-directories-first --color=auto --icons"
 
 # Use $XINITRC
-alias startx='startx $XINITRC'
+alias startx="startx $XINITRC"
 
 # Use color for commands when possible
 alias \
-    grep='grep --color=auto' \
-    diff='diff --color=auto' \
-    cat='bat --paging=never'
+    grep="grep --color=auto" \
+    diff="diff --color=auto" \
+    cat="bat --paging=never"
 
 # Set basic verbosity and misc. settings
 alias \
-    cp='cp -iv' \
-    mv='mv -iv' \
-    mkdir='mkdir -pv' \
-    rm='rm -Iv' \
-    lf='lfub'
+    cp="cp -iv" \
+    mv="mv -iv" \
+    mkdir="mkdir -pv" \
+    rm="rm -Iv" \
+    lf="lfub"
+
+# Shorten commonly used commands
+alias \
+    g="git" \
+    p="pacman" \
+    z="zathura" \
+    v="$EDITOR" \
+    e="$EDITOR"
+
